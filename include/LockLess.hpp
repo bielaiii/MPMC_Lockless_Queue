a11af982946be06f92d0d4fbe13d3daa97007eea
bielaiii <aminoyls@gmail.com>
2026-02-07 23:18:20 +0800

    fix some memory order

--- include/LockLess.hpp
+++ include/LockLess.hpp
@@ -5,6 +5,7 @@
 #include <atomic>
 #include <functional>
 #include <new>
+#include <print>
 namespace Tools {
 namespace detail {
 
@@ -16,84 +17,88 @@ namespace detail {
 #define INTEREFERENCE std::hardware_destructive_interference_size
 #endif
 
-template<typename Element>
-struct QueueElement {
-    
-};
+template <typename Element>
+struct QueueElement {};
 
 template <typename Element, size_t N>
 class Queue {
     char pad0[INTEREFERENCE];
     size_t size_{N};
     alignas(INTEREFERENCE) std::atomic<int> producer_ptr{};
-    alignas(INTEREFERENCE) std::atomic<int> inner_ptr;
+    alignas(INTEREFERENCE) std::atomic<int> inner_ptr{};
     alignas(INTEREFERENCE) std::atomic<int> consumer_ptr{};
     char pad1[INTEREFERENCE - sizeof(std::atomic<int>)];
     std::array<Element, N> inner_queue;
-public:
 
+public:
     void publish_enqueue(int idx) noexcept {
-        int old_value = idx;
-        int ticket = idx;
-        while(!producer_ptr.compare_exchange_strong(old_value, ticket,  std::memory_order_relaxed)) {
-            if (old_value == ticket + 1) {
-                
-                break;;
-            }
-        }
-        producer_ptr.fetch_add(1);
+        int expected = idx;
+        int desired  = idx + 1;
+
+        // here is linearization point,
+        while (!producer_ptr.compare_exchange_strong(
+            expected, desired, std::memory_order_release)) {}
     }
 
+    template <typename... Args>
+    bool enqueue(Args &&...args) noexcept {
+        auto ticket = inner_ptr.fetch_add(1, std::memory_order_acquire);
 
-    template<typename... Args>
-    bool enqueue(Args&&... args)noexcept {
-        auto ticket = inner_ptr.load(std::memory_order::acquire);
-        auto old_value = ticket;
-        ticket++;
-        new (static_cast<char*>(&inner_queue[ticket])) Element(std::forward<Args>(args)...);
-        inner_ptr.fetch_add(1,std::memory_order::acq_rel);
+        new (static_cast<void *>(&inner_queue[ticket]))
+            Element(std::forward<Args>(args)...);
+
         publish_enqueue(ticket);
-        return 1;
-    }  
-    
-    template<typename pushed_element>
-    bool enqueue(pushed_element&& element) noexcept {
-        
-        auto ticket = inner_ptr.fetch_add(1,std::memory_order::acquire);
-        auto old_value = ticket;
-        
-        ticket++;
+        return true;
+    }
+
+    template <typename pushed_element>
+    bool enqueue(pushed_element &&element) noexcept {
+        auto ticket = inner_ptr.fetch_add(1, std::memory_order_acquire);
+
         inner_queue[ticket] = std::forward<pushed_element>(element);
-        
+
         publish_enqueue(ticket);
-        return 1;
-        
+
+        return true;
     }
 
     int head_idx() noexcept {
-        return consumer_ptr.load(std::memory_order::acquire);
+        return consumer_ptr.load(std::memory_order_release);
     }
 
-    bool dequeue(Element &element) {
-        int check_last = consumer_ptr.fetch_add(1, std::memory_order_release);
-        if (check_last == size_ - 1) {
+    bool dequeue(Element &element) noexcept {
+
+        if (empty()) {
+            return false;
+        }
+
+        int last_one = consumer_ptr.fetch_add(1, std::memory_order_acquire);
+        if (last_one == size_ - 1) {
             consumer_ptr.store(0, std::memory_order_acquire);
         }
-        
-        element = std::move(inner_queue[check_last]);
-        return 1;
+
+        element = std::move(inner_queue[last_one]);
+        return true;
     }
+
+    // non-blocking
+    // only try once
     bool try_enqueue(Element element) {}
-    bool try_dequeue(Element & element) {}
+    bool try_dequeue(Element &element) {}
 
+
+    // assuming operation will be success anyway
     bool unsafe_enqueue(Element element) {}
-    bool unsafe_dequeue(Element& element) {}
+    bool unsafe_dequeue(Element &element) {}
 
-
-    bool empty() noexcept {
-        return consumer_ptr.load() == producer_ptr.load();
+    size_t size() noexcept {
+        return producer_ptr.load(std::memory_order_acquire) -
+               consumer_ptr.load(std::memory_order_acquire);
     }
-    size_t size() noexcept { return size_; }
+    bool empty() noexcept {
+        return consumer_ptr.load(std::memory_order_acquire) ==
+               producer_ptr.load(std::memory_order_acquire);
+    }
 };
 } // namespace detail
 } // namespace Tools
--- test/main.cpp
+++ test/main.cpp
@@ -7,25 +7,28 @@ using namespace std::chrono_literals;
 
 template <typename Element, size_t N>
 void running_func(Tools::detail::Queue<Element, N> &lockless_q, Element i) {
-    // std::this_thread::sleep_for(5s);
-    //std::print("value {}", i);
+
     lockless_q.enqueue(i);
 }
 
 int main() {
 
-    std::vector<std::jthread> pools(20);
-
     auto Queue_ = Tools::detail::Queue<int, 32>();
 
-    for (int i = 0; i < 4; i++) {
-        pools[i] = std::jthread(running_func<int, 32>, std::ref(Queue_), i);
+    {
+
+        std::vector<std::jthread> pools(20);
+        for (int i = 0; i < 4; i++) {
+            pools[i] = std::jthread(running_func<int, 32>, std::ref(Queue_), i);
+        }
     }
 
+    std::println("real sz : {}", Queue_.size());
+    std::print("===============\n");
     while (!Queue_.empty()) {
         int value;
         int i = Queue_.dequeue(value);
-        std::println("value {}", value);
+        std::println("ret : {} value {}", i, value);
     }
 
     return 0;
